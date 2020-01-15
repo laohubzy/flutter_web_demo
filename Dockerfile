70a2ee53d5779f06aa091aefbb18e97a602b27d0
虎牙 <laohubzy@163.com>
2020-01-15 14:25:05 +0800

    [feat]增加部署配置文件

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node
+
+
+RUN flutter packages get
+RUN flutter build web
+
+
+FROM nginx
+
+COPY build/web /usr/share/nginx/html/
+COPY conf.nginx /etc/nginx/nginx.conf
+
+EXPOSE 80
---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+image_version=`date +%Y%m%d%H%M`;
+# 关闭shop_admin容器
+docker stop flutter_web || true;
+# 删除shop_admin容器
+docker rm flutter_web || true;
+# 删除shop/admin镜像
+docker rmi --force $(docker images | grep shop/admin | awk '{print $3}')
+# 构建shop/admin:$image_version镜像
+docker build . -t shop/admin:$image_version;
+# 查看镜像列表
+docker images;
+# 基于shop/admin 镜像 构建一个容器 flutter_web
+docker run -p 9527:80 -d --name flutter_web shop/admin:$image_version;
+# 查看日志
+docker logs flutter_web;
+#删除build过程中产生的镜像    #docker image prune -a -f
+docker rmi $(docker images -f "dangling=true" -q)
+# 对空间进行自动清理
+docker system prune -a -f
