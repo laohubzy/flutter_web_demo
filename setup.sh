024c42b89248426fdbf87492c032d4779b45a281
虎牙 <laohubzy@163.com>
2020-01-15 14:56:38 +0800

    re

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 image_version=`date +%Y%m%d%H%M`;
-# 关闭shop_admin容器
-docker stop flutter_web || true;
-# 删除shop_admin容器
-docker rm flutter_web || true;
-# 删除shop/admin镜像
-docker rmi --force $(docker images | grep shop/admin | awk '{print $3}')
+# # 关闭shop_admin容器
+# docker stop flutter_web || true;
+# # 删除shop_admin容器
+# docker rm flutter_web || true;
+# # 删除shop/admin镜像
+# docker rmi --force $(docker images | grep flutter/web | awk '{print $3}')
 # 构建shop/admin:$image_version镜像
-docker build . -t shop/admin:$image_version;
+docker build . -t flutter/web:$image_version;
 # 查看镜像列表
 docker images;
 # 基于shop/admin 镜像 构建一个容器 flutter_web
-docker run -p 9527:80 -d --name flutter_web shop/admin:$image_version;
+docker run -p 9527:80 -d --name flutter_web flutter/web:$image_version;
 # 查看日志
 docker logs flutter_web;
 #删除build过程中产生的镜像    #docker image prune -a -f
