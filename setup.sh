c91d349bf86d2925bb933d798b075ef8b9a5c521
虎牙 <laohubzy@163.com>
2020-01-19 11:21:18 +0800

    re

--- setup.sh
+++ setup.sh
@@ -17,33 +17,21 @@ image_version=`date +%Y%m%d%H%M`;
 # docker build . -t flutter/web:$image_version;
 # docker run --rm -it -v ${PWD}:/build --workdir /build cirrusci/flutter:beta flutter build web;
 
-# 构建flutter/web镜像
-docker build . -t flutter/web:$image_version;
-# FROM cirrusci/flutter
-# 查看镜像列表 re
-docker images;
-
-# 基于flutter/web 镜像 构建一个容器 flutter_web
-docker run -p 9527:80 -d --name flutter_web flutter/web:$image_version;
-# 查看日志
-docker logs flutter_web;
-
-#删除build过程中产生的镜像 
-docker rmi $(docker images -f "dangling=true" -q)
-
-# 对空间进行自动清理
-docker system prune -a -f
-# # # 删除shop/admin镜像
-# # docker rmi --force $(docker images | grep flutter/web | awk '{print $3}')
-# # 构建shop/admin:$image_version镜像
+# # 构建flutter/web镜像
 # docker build . -t flutter/web:$image_version;
-# # 查看镜像列表
+# # FROM cirrusci/flutter
+# # 查看镜像列表 re
 # docker images;
-# # 基于shop/admin 镜像 构建一个容器 flutter_web
+
+# # 基于flutter/web 镜像 构建一个容器 flutter_web
 # docker run -p 9527:80 -d --name flutter_web flutter/web:$image_version;
 # # 查看日志
 # docker logs flutter_web;
-# #删除build过程中产生的镜像    #docker image prune -a -f
+
+# #删除build过程中产生的镜像 
 # docker rmi $(docker images -f "dangling=true" -q)
+
 # # 对空间进行自动清理
 # docker system prune -a -f
+
+docker run --rm -it -v ${PWD}:/build --workdir /build cirrusci/flutter:stable flutter  build web
