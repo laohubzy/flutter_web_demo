0fc65a79456a8d329efdca617aef9ed300338de8
虎牙 <laohubzy@163.com>
2020-02-10 13:37:34 +0800

    re

--- setup.sh
+++ setup.sh
@@ -16,8 +16,8 @@ image_version=`date +%Y%m%d%H%M`;
 # docker pull cirrusci/flutter;
 # docker build . -t flutter/web:$image_version;
 # docker run --rm -it -v ${PWD}:/build --workdir /build cirrusci/flutter:beta flutter build web;
-flutter config --enable-web;
-flutter build web;
+# flutter config --enable-web;
+# flutter build web;
 # 构建flutter/web镜像
 docker build . -t flutter/web:$image_version;
 # FROM cirrusci/flutter
